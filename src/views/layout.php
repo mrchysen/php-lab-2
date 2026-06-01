6f32e209bb159b711d08dcb49acea99e2f25d571
mrchysen <mrchysen@mail.ru>
2026-06-01 22:34:20 +0700

    make Trump dance again

--- src/views/layout.php
+++ src/views/layout.php
@@ -13,6 +13,8 @@
         <?php echo $content; ?>
     </main>
 
+    <video src="vid/trump.mp4" style="position: absolute; bottom: 10px; right: 10px; padding: 5px 10px; width: 200px; height: 400px" autoplay loop muted controls></video>
+
     <script src="./scripts/api.js"></script>
     <script src="./scripts/start-up.js"></script>
     <script src="./scripts/table-view-functions.js"></script>
